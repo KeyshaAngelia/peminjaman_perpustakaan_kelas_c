e719fcfdc19a8094854b3c28aa99d7158029a2d0
Radhesta <radestaghifari@gmail.com>
2024-01-31 14:33:38 +0700

    first commit

---
+++ lib/app/modules/book/controllers/book_controller.dart
@@ -0,0 +1,52 @@
+import 'package:dio/dio.dart';
+import 'package:get/get.dart';
+import 'package:peminjaman_perpustakan_kelas_c/app/data/model/response_book.dart';
+
+import '../../../data/constant/endpoint.dart';
+import '../../../data/provider/app_provider.dart';
+
+class BookController extends GetxController with StateMixin<List<DataBook>>{
+
+  @override
+  void onInit() {
+    super.onInit();
+    getData();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  getData() async {
+    change(null, status: RxStatus.loading());
+    try {
+      final response = await ApiProvider.instance().get(Endpoint.book);
+      if (response.statusCode == 200) {
+        final ResponseBook responseBook = ResponseBook.fromJson(response.data);
+        if(responseBook.data!.isEmpty){
+          change(null, status: RxStatus.empty());
+        }else{
+          change(responseBook.data, status: RxStatus.success());
+        }
+      } else {
+        change(null, status: RxStatus.error("Gagal mengambil data"));
+      }
+    } on DioException catch (e) {
+      if (e.response != null) {
+        if (e.response?.data != null) {
+          change(null, status: RxStatus.error(" dio ${e.response?.data['message']}"));
+        }
+      } else {
+        change(null, status: RxStatus.error("cek"+ (e.message ?? "")));
+      }
+    } catch (e) {
+      change(null, status: RxStatus.error("catch "+e.toString()));
+    }
+  }
+}
---
+++ lib/app/modules/peminjaman/views/peminjaman_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:peminjaman_perpustakan_kelas_c/app/data/MODEL/response_pinjam.dart';
+
+import '../../../data/MODEL/response_book.dart';
+import '../controllers/peminjaman_controller.dart';
+
+class PeminjamanView extends GetView<PeminjamanController> {
+  const PeminjamanView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('PeminjamanView'),
+          centerTitle: true,
+        ),
+        body: controller.obx((state) => ListView.separated(
+          itemCount:  state!.length,
+          itemBuilder: (context, index){
+            DataPinjam dataPinjam= state [index];
+            return ListTile(
+              title: Text("Buku ${dataPinjam.book?.judul}"),
+              subtitle: Text("Tanggal Pinjam: ${dataPinjam.tanggalPinjam}\ntanggal kembali: ${dataPinjam.tanggalKembali}") ,
+            );
+          },
+          separatorBuilder: (context, index)=> Divider(),
+        ))
+    );
+  }
+}
