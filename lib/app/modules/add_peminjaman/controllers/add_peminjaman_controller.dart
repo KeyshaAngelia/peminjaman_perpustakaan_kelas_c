1e6c38be7acaa48b71260019decaa040a6de383f
Radhesta <radestaghifari@gmail.com>
2024-02-01 14:23:45 +0700

    first commit

--- lib/app/modules/add_peminjaman/controllers/add_peminjaman_controller.dart
+++ lib/app/modules/add_peminjaman/controllers/add_peminjaman_controller.dart
@@ -1,24 +1,19 @@
-import 'dart:html';
-
-import 'package:dio/dio.dart' as dio;
-import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 import 'package:get/get.dart';
+
+import '../../../data/MODEL/response_pinjam.dart';
 import '../../../data/constant/endpoint.dart';
 import '../../../data/provider/app_provider.dart';
 import '../../../data/provider/storage_provider.dart';
 
-class AddPeminjamanController extends GetxController {
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  final TextEditingController user_idController = TextEditingController();
-  final TextEditingController book_idController = TextEditingController();
-  final TextEditingController tanggal_pinjamController = TextEditingController();
-  final TextEditingController tanggal_kembaliController = TextEditingController();
-  final loading = false.obs;
+class PeminjamanController extends GetxController with StateMixin<List<DataPinjam>>{
+  //TODO: Implement PeminjamanController
 
   final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+    getPinjam();
   }
 
   @override
@@ -31,41 +26,31 @@ class AddPeminjamanController extends GetxController {
     super.onClose();
   }
 
-  addpinjam() async {
-    loading(true);
+  getPinjam() async {
+    change(null, status: RxStatus.loading());
     try {
-      FocusScope.of(Get.context!).unfocus(); //nge close keyboard
-      formKey.currentState?.save();
-      if (formKey.currentState!.validate()) {
-        final response = await ApiProvider.instance().post(Endpoint.pinjam,
-            data: {
-              'tanggal_pinjam': tanggal_pinjamController.text.toString(),
-              'tanggal_kembali': tanggal_kembaliController.text.toString(),
-              'user_id': int.parse(StorageProvider.read(StorageKey.iduser)),
-              'book_id': int.parse(Get.parameters['id'].toString())
-            });
-        if (response.statusCode == 201) {
-          Get.back();
+      final response = await ApiProvider.instance().get(Endpoint.pinjam+"/${StorageProvider.read(StorageKey.iduser)}" );
+      if (response.statusCode == 200) {
+        final ResponsePinjam responseBook = ResponsePinjam.fromJson(response.data);
+        if (responseBook.data!.isEmpty) {
+          change(null, status: RxStatus.empty());
         } else {
-          Get.snackbar('Sorry', 'Login Gagal', backgroundColor: Colors.orange);
-        }
-      }
-      loading(false);
-    } on dio.DioException catch (e) {
-      loading(false);
-      if (e.response != null) {
-        if (e.response?.data != null) {
-          Get.snackbar('Sorry', '${e.response?.data['message']}',
-              backgroundColor: Colors.orange);
+          change(responseBook.data, status: RxStatus.success());
         }
       } else {
-        Get.snackbar('Sorry', e.message ?? '', backgroundColor: Colors.red);
+        change(null, status: RxStatus.error("Gagal mengambil data"));
+      }
+    } on DioException catch (e) {
+      if (e.response != null) {
+        if (e.response?.data != null) {
+          change(
+              null, status: RxStatus.error("${e.response?.data['message']}"));
+        }
+      } else {
+        change(null, status: RxStatus.error(e.message ?? ""));
       }
     } catch (e) {
-      loading(false);
-      Get.snackbar('Error', e.toString(), backgroundColor: Colors.red);
+      change(null, status: RxStatus.error(e.toString()));
     }
   }
-
-  void increment() => count.value++;
 }
