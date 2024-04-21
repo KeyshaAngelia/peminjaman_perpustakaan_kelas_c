88d82dd1777c946a510b6f1c3779b24eed06ebd8
eganaww <geminggg81@gmail.com>
2024-04-21 23:05:14 +0700

    Add Home Page

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,12 +1,18 @@
 import 'package:get/get.dart';
+import 'package:dio/dio.dart';
 
-class HomeController extends GetxController {
+import '../../../data/constant/endpoint.dart';
+import '../../../data/model/response_book.dart';
+import '../../../data/provider/app_provider.dart';
+
+class HomeController extends GetxController with StateMixin<List<DataBook>> {
   //TODO: Implement HomeController
 
   final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+    getData();
   }
 
   @override
@@ -20,4 +26,31 @@ class HomeController extends GetxController {
   }
 
   void increment() => count.value++;
+
+  getData() async {
+    change(null, status: RxStatus.loading());
+    try {
+      final response = await ApiProvider.instance().get(Endpoint.book);
+      if (response.statusCode == 200) {
+        final ResponseBook responseBook = ResponseBook.fromJson(response.data);
+        if(responseBook.data!.isEmpty){
+          change(null, status: RxStatus.empty());
+        }else{
+          change(responseBook.data, status: RxStatus.success());
+        }
+      } else {
+        change(null, status: RxStatus.error("Gagal mengambil data"));
+      }
+    } on DioException catch (e) {
+      if (e.response != null) {
+        if (e.response?.data != null) {
+          change(null, status: RxStatus.error(" dio ${e.response?.data['message']}"));
+        }
+      } else {
+        change(null, status: RxStatus.error("cek"+ (e.message ?? "")));
+      }
+    } catch (e) {
+      change(null, status: RxStatus.error("catch "+e.toString()));
+    }
+  }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:another_carousel_pro/another_carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../routes/app_pages.dart';
@@ -8,17 +9,131 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(onPressed: ()=>Get.toNamed(Routes.BOOK), child: Text("Buku")),
-            ElevatedButton(onPressed: ()=>Get.toNamed(Routes.ADD_PEMINJAMAN), child: Text("Peminjaman")),
-          ],
-        ),
+      // appBar: AppBar(
+      //   title: const Text('HomeView'),
+      //   centerTitle: true,
+      // ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 25),
+          height: MediaQuery.of(context).size.height,
+          color: Color(0xff1B255A),
+          child: Column(
+            children: [
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/img/POLCA-logo.jpeg', // Ganti dengan path menuju gambar logo
+                       width: 65,// Sesuaikan dengan ukuran yang diinginkan
+                    ),
+                    Text('Polca Library', style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    )),
+                    Ink(
+                      padding: EdgeInsets.only(left: 30, right: 10),
+                      decoration: BoxDecoration(
+                        color: Colors.white
+                      ),
+                      child: IconButton(
+                        icon: const Icon(Icons.search),
+                        color: Colors.black,
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Color(0xff8FBEEA)),
+                          padding: MaterialStateProperty.all(EdgeInsets.only(left: 10, right: 80)
+                          )
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    Image.asset(
+                      'assets/img/photo-profile.png', // Ganti dengan path menuju gambar logo
+                      width: 40,// Sesuaikan dengan ukuran yang diinginkan
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20,),
+              Container(
+                child:
+                SizedBox(
+                    height: 200.0,
+                    width: 350.0,
+                    child: AnotherCarousel(
+                      images: [
+                        ExactAssetImage("assets/img/slider-1.jpeg"),
+                        ExactAssetImage("assets/img/slider-2.jpeg"),
+                      ],
+                      showIndicator: false,
+                      borderRadius: true,
+                      moveIndicatorFromBottom: 180.0,
+                      noRadiusForIndicator: true,
+                      overlayShadow: true,
+                      overlayShadowSize: 0,
+                    )
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 25),
+                      alignment: Alignment.centerLeft,
+                      child: Text('Top Picks For You',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+
+                        ),),
+                    ),
+                    Container(
+                      height: 240,
+                      padding: EdgeInsets.only(left: 20),
+                      child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: controller.obx((state) => ListView.builder(
+                              itemCount: state?.length,
+                              scrollDirection: Axis.horizontal,
+                              itemBuilder: (context, index) {
+                                return InkWell(
+                                  onTap: () {},
+                                  child: Container(
+                                    margin: const EdgeInsets.only(right: 10),
+                                    child: Column(
+                                      children: [
+                                        Container(
+                                          width: 130,
+                                          height: 160,
+                                          decoration: BoxDecoration(
+                                            image: DecorationImage(
+                                              image:
+                                              AssetImage('assets/img/cover-not-found.jpeg'),
+                                              fit: BoxFit.cover,
+                                            ),),
+                                        ),
+                                        Text(
+                                          state![index].judul ?? '',
+                                          style: TextStyle(
+                                              fontSize: 15, color: Colors.white),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              }))),
+                    ),
+                  ],
+                ),
+              ),
+
+            ],
+          ),
+        )
       ),
     );
   }
